19c3a7e6314e19967b867996ccdc31496cc70c40
kern-gt <gtoscompressor1209@gmail.com>
2018-06-22 20:06:10 +0900

    プロジェクト作成

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MySettings_FreeRTOSv10_0_1_for_RX65N_with_CC-RX
+# FreeRTOS Ver10.0.1 RenesasRX65N用
+
+## [注意]これは私用のプロジェクトです。
+勉強用はこちらをどうぞ  
+ https://github.com/kern-gt/Demo_FreeRTOS_v10.0.1_for_Renesas_RX65N_with_CSplus_CC-RX  
---
+++ main.c
@@ -0,0 +1,72 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+＊　ファイル名： main.c Ver1.00
+＊　内容　　　： FreeRTOSv10.0.1デモプロジェクト（RenesasRX65N用）
+＊　作成日　　： 2018/3/11
+＊　作成者　　： kern-gt
+
+	文字エンコード：UTF-8
+
+	FreeRTOS:v10.0.1　(RX600 RXv2)
+	開発環境：CS+forCC V6.01.00
+	コンパイラ：CC-RX V2.08.00
+	CPUボード：TARGET BOARD for RX65N(RTK5RX65N0C00000BR)
+	CPU：R5F565NEDDFP
+
+	デモ内容：
+	CPUボード上のLED0を1Hz、LED1を5Hzで点滅させる2つのタスクを動かします。
+
+	クロック発生回路とポート初期化をスマートコンフィグレータで設定しています。
+	FreeRTOSではカーネルタイマにCMT0,システムコールにソフトウェア割込み(SWINT)を使用している
+	ので、その機能は使用しないでください。
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**/
+/**----------------------------------------------------------------------------
+<<利用ファイルのヘッダ>>
+-----------------------------------------------------------------------------**/
+/* Renesas includes. */
+/*#include "r_cg_macrodriver.h"*/
+/*#include "r_cg_userdefine.h"*/
+#include "iodefine.h"
+
+/* Kernel includes. */
+#include "FreeRTOS/FreeRTOS.h"
+#include "FreeRTOS/task.h"
+
+/* Prototype */
+void main(void);
+void vTask1(void *pvParameters);
+void vTask2(void *pvParameters);
+void vApplicationSetupTimerInterrupt(void);
+
+
+void main(void)
+{
+	/*タスク生成*/
+	xTaskCreate(vTask1,"Task1",100,NULL,1,NULL);
+	xTaskCreate(vTask2,"Task2",100,NULL,1,NULL);
+	
+	/* タスクスケジューラ起動*/
+	vTaskStartScheduler();
+
+	/*ここには戻ってこないはず*/
+	while(1);
+}
+
+
+/***タスク定義*******************************************************************/
+void vTask1(void *pvParameters)
+{
+	while(1) {
+		/*CPUボード上のLED0を1Hzで点滅*/
+		PORTD.PODR.BIT.B6 = ~PORTD.PODR.BIT.B6;
+		vTaskDelay(500/portTICK_PERIOD_MS);
+	}
+}
+
+void vTask2(void *pvParameters)
+{
+	while(1) {
+		/*CPUボード上のLED1を5Hzで点滅*/
+		PORTD.PODR.BIT.B7 = ~PORTD.PODR.BIT.B7;
+		vTaskDelay(100/portTICK_PERIOD_MS);
+	}
+}
